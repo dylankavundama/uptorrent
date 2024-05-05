c0f657d314e77db1da4056fda82836183ba2fb44
Dylankavvundama <85756267+dylankavundama@users.noreply.github.com>
2024-05-05 17:09:49 +0200

    kkk

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:admin_ib/login/login.dart';
- 
 
 // import 'package:flutter_native_splash/flutter_native_splash.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
-
   await Firebase.initializeApp();
   await Future.delayed(const Duration(seconds: 1));
   // FlutterNativeSplash.remove();
   // SystemChrome.setSystemUIOverlayStyle(
-  //   const SystemUiOverlayStyle(
-  //     statusBarColor: Colors.teal,
-  //     statusBarBrightness: Brightness.light,
-  //   ),
-  // );
+
   runApp(
     const MyApp(),
   );
