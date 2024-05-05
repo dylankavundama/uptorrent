e3e8ed7ce8134d037240ed0d42829694c384e9a4
Dylankavvundama <85756267+dylankavundama@users.noreply.github.com>
2024-05-05 17:11:41 +0200

    kkkk

--- lib/Util/style.dart
+++ lib/Util/style.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
-
 // ignore: non_constant_identifier_names
 Color CouleurPrincipale = Colors.orange;
-
 // ignore: non_constant_identifier_names
 TextStyle TitreStyle = GoogleFonts.abel(fontSize: 24, color: Colors.black);
-
 // ignore: non_constant_identifier_names
 TextStyle TitreStyleWhite = GoogleFonts.abel(fontSize: 24, color: Colors.white);
-
 // ignore: non_constant_identifier_names
 TextStyle SousTStyle =
     GoogleFonts.actor(fontSize: 18, color: Colors.orange.shade400);
-
 // ignore: non_constant_identifier_names
 TextStyle DescStyle = GoogleFonts.abel(fontSize: 19, color: Colors.black);
-
 // ignore: non_constant_identifier_names
 var Adress_IP = 'https://royalrisingplus.com/upt';
