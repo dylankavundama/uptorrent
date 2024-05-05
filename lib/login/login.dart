0b7e8d894d0e66724eb9aac34c217337d61342e7
Dylankavvundama <85756267+dylankavundama@users.noreply.github.com>
2024-05-05 17:23:25 +0200

    kkkk

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -8,6 +8,7 @@ class LoginHome extends StatefulWidget {
   const LoginHome({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _LoginHomeState createState() => _LoginHomeState();
 }
 
